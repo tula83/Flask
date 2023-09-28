bfeede558b723395a7ceaed0ee32b910dfccf5a0
tula83 <131385747+tula83@users.noreply.github.com>
2023-09-28 13:24:45 +0530

    Create mainProgram.py

---
+++ ModulePython/mainProgram.py
@@ -0,0 +1,12 @@
+import mathOperations as mops
+
+
+result_add=mops.add(12,2)
+result_sub=mops.subtract(12,2)
+result_mult=mops.multiply(12,2)
+result_divide=mops.divide(12,0)
+
+print(f'addition result is{result_add}')
+print(f'subtraction result is{result_sub}')
+print(f'multiplication result is{result_mult}')
+print(f'division result is{result_divide}')
