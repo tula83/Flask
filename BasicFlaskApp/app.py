93490ef123bbf07fc50fed6a63899cbe2b1a9df6
tula83 <131385747+tula83@users.noreply.github.com>
2023-10-13 10:13:01 +0530

    Create app.py

---
+++ BasicFlaskApp/app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+app=Flask(__name__)
+tasks=[]
+
+@app.route("/")
+def index():
+    return render_template('./index.html',tasks=tasks)
+
+
+@app.route('/add_task',methods=['POST'])
+def add_task():
+    task=request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('Index'))
+
+@app.route('/home')
+def get():
+    return render_template('app.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)                               
